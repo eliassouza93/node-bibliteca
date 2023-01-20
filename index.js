3651a4c66e146e276f16a7c2d0b6da943b4c63b0
Elias de Souza <elias99-souza@hotmail.com>
2023-01-20 09:14:45 -0300

    'subindoArquivos'

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Olá, Mundo!')
+import fs from 'fs'
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8'
+    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+        console.log(texto)
+    })
+
+}
