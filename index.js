a007a452c6997b5697db4520e4f541ac51099d30
Elias de Souza <elias99-souza@hotmail.com>
2023-01-20 10:07:59 -0300

    'assincronaFuncao'

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 import * as fs from 'fs';
 
- 
+
+function trataErro(erro) {
+
+    throw new Error(erro, 'Arquivo não encontrado!')
+
+}
 
 function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8'
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
-        console.log(texto)
-    })
-
+    fs.promises.readFile(caminhoDoArquivo, encoding)
+        .then((texto) => console.log(texto))
+        .catch((erro) => trataErro(erro)) // ou .cath(trataErro)
 }
+
+//function pegaArquivo(caminhoDoArquivo) {
+//   const encoding = 'utf-8'
+//   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//       if (erro) {
+//           trataErro(erro)
+//
+//       }
+//       console.log(texto)
+//   })
+
 
 pegaArquivo('./arquivos/texto.md')
