b306af33f346c19d030ebf4a9065e4997250dd07
Elias de Souza <elias99-souza@hotmail.com>
2023-01-23 12:19:51 -0300

    'atAwait'

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ function trataErro(erro) {
     throw new Error(erro, 'Arquivo não encontrado!')
 
 }
+//async/await
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8'
-    fs.promises.readFile(caminhoDoArquivo, encoding)
-        .then((texto) => console.log(texto))
-        .catch((erro) => trataErro(erro)) // ou .cath(trataErro)
+function pegaArquivo(caminhoDoArquivo){
+    const ecoding = 'utf-8'
+    const texto = fs.promises
+    .readFile(caminhoDoArquivo, ecoding)
+    console.log(texto)
+
+    
 }
 
+
+
+// sempre quando falamos de promisses, estamos falando de codigo assincrono
+//promises com then()
 //function pegaArquivo(caminhoDoArquivo) {
+//    const encoding = 'utf-8'
+//    fs.promises
+//        .readFile(caminhoDoArquivo, encoding)
+//        .then((texto) => console.log(texto))
+//        .catch((erro) => trataErro(erro)) // ou .cath(trataErro)
+//}
+//
+//function pegaArquivo(caminhoDoArquivo) { (versao 5)
 //   const encoding = 'utf-8'
 //   fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
 //       if (erro) {
