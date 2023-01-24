826eb157a960efc4c6fcc138b3daf1c1233fcb87
Elias de Souza <elias99-souza@hotmail.com>
2023-01-24 10:25:31 -0300

    'capturas'

--- index.js
+++ index.js
@@ -1,40 +1,30 @@
 import * as fs from 'fs';
 
-const textoTeste = 'São geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()` em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.)'
-
 function extraiLinks(texto) {
     const regex = /\[[^[\]]*?\]\(https?:\/\/[^\s?#.].[^\s]*\)/gm;
-    const capturas = texto.matchAll(regex)
-    console.log(capturas)
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({ [captura[1]]: captura[2] }))
 
+    return resultados;
 }
 
-extraiLinks(textoTeste)
-
-
 function trataErro(erro) {
-
-    throw new Error(erro, 'Arquivo não encontrado!')
-
+    throw new Error(erro.code, 'Arquivo não encontrado!')
 }
 
 //async/await
 
 async function pegaArquivo(caminhoDoArquivo) {
     try {
-        const ecoding = 'utf-8'
-        const texto = await fs.promises
-            .readFile(caminhoDoArquivo, ecoding)
-        console.log(texto)
-
-    } catch (error) {
-        console.log(error, 'erro caminho não encontrado!!')
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(extraiLinks(texto))
+    } catch (erro) {
+        trataErro(erro)
     }
-
 }
- 
 
-
+pegaArquivo('./arquivos/texto.md')
 
 // sempre quando falamos de promisses, estamos falando de codigo assincrono
 //promises com then()
@@ -56,5 +46,5 @@ async function pegaArquivo(caminhoDoArquivo) {
 //       console.log(texto)
 //   })  \(https?:\/\/[^\s?#.]*        \(https?:\/\/[^\s?#.].[^\s]*\)
 
- 
+
 
