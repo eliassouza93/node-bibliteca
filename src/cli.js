2958a04e3f7b111b2a7c24b3dc812e5c62f66c32
Elias de Souza <elias99-souza@hotmail.com>
2023-02-04 08:26:03 -0300

    'async'

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,27 @@
 import pegaArquivo from "./index.js";
+import fs from 'fs'
 
 const caminho = process.argv
- 
-async function processaTexto(caminho){
-    const resultado = await pegaArquivo(caminho[2])
-    console.log('Lista de links ' , resultado)
+
+function imprimeLista(resultado) {
+    console.log('lista de links', resultado)
+}
+
+async function processaTexto(argumentos) {
+    const caminho = argumentos[2]
+
+    if (fs.lstatSync(caminho).isFile()) {
+        const resultado = await pegaArquivo(argumentos[2])
+        imprimeLista(resultado)
+
+    } else if (fs.lstatSync(caminho).isDirectory()) {
+        const arquivos = await fs.promises.readdir(caminho)
+        arquivos.forEach(async (nomedeArquivo) => {
+            const lista = await pegaArquivo(caminho + '/' + nomedeArquivo)
+            imprimeLista(lista)
+        })
+    }
+
 }
 
 processaTexto(caminho)
